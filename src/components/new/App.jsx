c0edc777937ebee8051dc8d3459f536850614b0b
jpreslov <lovett_jp@outlook.com>
2021-02-24 14:15:24 -0600

    added hooks example

---
+++ src/components/new/App.jsx
@@ -0,0 +1,36 @@
+//this one may not be fully fleshed out, just tried to throw together a quick example
+
+import React, { useState, useEffect } from 'react';
+import Greeter from './Greeter';
+
+const App = (props) => {
+  const [loaded, setLoaded] = useState(false);
+  const [username, setUsername] = useState('');
+
+  //   useEffect(() => {
+  //       setLoaded(!isLoaded);
+  //   }, []);
+
+  let handleClick = () => setLoaded(!loaded);
+
+  if (loaded) {
+    return (
+      <div>
+        <Greeter name="Bobby Deniro" phrase="U talkin 2 me?" />
+        <Greeter name="Joe PEsci" phrase="Funny how?" />
+        <Greeter name="Thomas the Cat" phrase="Ahhhhahahahooowwwww" />
+        <input type="text" onChange={(e) => setUsername(e.target.value)} />
+        <p>You are logging in as: {username}</p>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <h1>Loading...</h1>
+        <button onClick={() => handleClick()}>Click to load page</button>
+      </>
+    );
+  }
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+//keep this line uncommented for class component
 import App from './components/App'
+
+//uncomment this line for functional component & hooks
+// import App from './components/new/App'
 
 ReactDOM.render(<App text="What's up this is text from index.js passed as a prop"/>, document.getElementById('root'))
